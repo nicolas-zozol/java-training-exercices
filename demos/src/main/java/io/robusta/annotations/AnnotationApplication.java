3f1fff09aee97fcf525d6f714f6b004a19318645
Nicolas Zozol <nz@robusta.io>
2016-11-26 14:23:57 +0100

    Demos : Working on annotations

---
+++ demos/src/main/java/io/robusta/annotations/AnnotationApplication.java
@@ -0,0 +1,11 @@
+package io.robusta.annotations;
+
+/**
+ * Created by Nicolas on 22/11/2016.
+ */
+public class AnnotationApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ demos/src/main/java/io/robusta/annotations/Slow.java
@@ -0,0 +1,7 @@
+package io.robusta.annotations;
+
+/**
+ * Created by Nicolas on 22/11/2016.
+ */
+public class Slow {
+}
