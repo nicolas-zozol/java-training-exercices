0ae563bf51f2941e34b6b31530976377f5fb82ab
Nicolas Zozol <nz@robusta.io>
2016-11-27 16:04:16 +0100

    Java-base : Demo : starting lists

---
+++ demos/src/main/java/io/robusta/people/collection/GenericProblemApplication.java
@@ -0,0 +1,29 @@
+package io.robusta.people.collection;
+
+import io.robusta.people.forum.Comment;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Robusta Code
+ */
+public class GenericProblemApplication {
+
+    public static void main(String[] args) {
+
+        ArrayList liste = new ArrayList();
+        Comment c1 = new Comment("Toto");
+
+        //Adding comment and strings
+        liste.add(c1);
+        liste.add("Something");
+
+        for (int i=0; i<liste.size();i++){
+            Comment c = (Comment) liste.get(i);
+            System.out.println(c.html());
+
+            // try instanceof
+        }
+
+    }
+}
