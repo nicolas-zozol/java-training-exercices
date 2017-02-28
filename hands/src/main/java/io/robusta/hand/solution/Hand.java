936809b04e7ee1158064c7fc798ad29b0930fe3f
Nicolas Zozol <nzozol@gmail.com>
2017-02-28 15:36:40 +0100

    hands: more comments

--- hands/src/main/java/io/robusta/hand/solution/Hand.java
+++ hands/src/main/java/io/robusta/hand/solution/Hand.java
@@ -13,99 +13,108 @@ import io.robusta.hand.interfaces.IDeck;
 import io.robusta.hand.interfaces.IHand;
 import io.robusta.hand.interfaces.IHandResolver;
 
-public class Hand extends TreeSet<Card> implements IHand{
+public class Hand extends TreeSet<Card> implements IHand {
 
-	
-	private static final long serialVersionUID = 7824823998655881611L;
 
-	@Override
-	public Set<Card> changeCards(IDeck deck, Set<Card> cards) {
-		// For exemple remove three cards from this hand
+    private static final long serialVersionUID = 7824823998655881611L;
+
+    @Override
+    public Set<Card> changeCards(IDeck deck, Set<Card> cards) {
+        // For exemple remove three cards from this hand
         // , and get 3 new ones from the Deck
         // returns the new given cards
-		return null;
-	}
+        return null;
+    }
 
 
-	@Override
-	public boolean beats(IHand villain) {
-		return false;
-	}
+    /**
+     * beats is the same than compareTo, but with a nicer name.
+     * The problem is that it does not handle equality :(
+     * @param villain
+     * @return
+     */
+    @Override
+    public boolean beats(IHand villain) {
+        return false;
+    }
 
-	@Override
-	public IHand getHand() {
-		return this;
-	}
+    @Override
+    public IHand getHand() {
+        return this;
+    }
 
-	@Override
-	public HandClassifier getClassifier() {
-		
-		return this.getValue().getClassifier();
-	}
+    @Override
+    public HandClassifier getClassifier() {
 
-	@Override
-	public boolean isStraight() {
+        return this.getValue().getClassifier();
+    }
 
-		return false;
-	}
+    @Override
+    public boolean isStraight() {
 
-	@Override
-	public boolean isFlush() {
+        return false;
+    }
 
-		return true;
-	}
+    @Override
+    public boolean isFlush() {
+
+        return true;
+    }
 
 
     /**
      * Returns number of identical cards
      * 5s5cAd2s3s  has two cardValue of 5
      */
-	@Override
-	public int number(int cardValue) {
-		int result = 0;
-		for (Card current : this){
-			if (current.getValue() == cardValue){
-				result++;
-			}
-		}
-		return result;
-	}
+    @Override
+    public int number(int cardValue) {
+        int result = 0;
+        for (Card current : this) {
+            if (current.getValue() == cardValue) {
+                result++;
+            }
+        }
+        return result;
+    }
 
 
     /**
      * The fundamental map
      * Check the tests and README to understand
      */
-	@Override
-	public Map<Integer, List<Card>> group() {
-		HashMap<Integer, List<Card> > map = new HashMap<>();
+    @Override
+    public Map<Integer, List<Card>> group() {
+        HashMap<Integer, List<Card>> map = new HashMap<>();
 
         // fill the map
 
-		return map;
-	}
+        return map;
+    }
 
 
     // different states of the hand
     // Using stateful variables. We need to fill this, then use it before.
-	int levelValue = 0;
-	int secondValue = 0;
-	TreeSet<Card> singleCards = new TreeSet<>();
+    int levelValue = 0;
+    // Needed with two pairs or full
+    int secondValue = 0;
+    // Put all cards for flush or highCard ;
+    TreeSet<Card> singleCards = new TreeSet<>();
 
 
     /**
      * return all single cards not used to build the classifier
+     *
      * @param map
      * @return
      */
-    TreeSet<Card> getSingleCards(Map<Integer, List<Card>> map){
+    TreeSet<Card> getSingleCards(Map<Integer, List<Card>> map) {
+        // method is done, DO NOT TOUCH !
         TreeSet<Card> singleCards = new TreeSet<>();
         // May be adapted at the end of project:
         // if straight or flush : return empty
         // If High card, return 4 cards
-
-        for (List<Card> group :map.values()){
-            if (group.size() ==1){
+        for (List<Card> group : map.values()) {
+            if (group.size() == 1) {
                 singleCards.add(group.get(0));
             }
         }
@@ -113,101 +122,96 @@ public class Hand extends TreeSet<Card> implements IHand{
     }
 
 
-	@Override
-	public boolean isPair() {
-
-			return false;	
-
-		
-	}
+    @Override
+    public boolean isPair() {
+        return false;
+    }
 
 
-	@Override
-	public boolean isDoublePair() {
-		return false;
-	}
+    @Override
+    public boolean isDoublePair() {
+        return false;
+    }
 
 
-	@Override
-	public boolean isHighCard() {
-
-		return true;
-	}
-
-
-	@Override
-	public boolean isTrips() {
-
-		return false;
-	}
+    @Override
+    public boolean isHighCard() {
+        return true;
+    }
 
 
+    @Override
+    public boolean isTrips() {
 
-	@Override
-	public boolean isFourOfAKind() {
-
-			return false;	
-
-		
-		
-	}
+        return false;
+    }
 
 
-	
-	@Override
-	public boolean isFull() {
-		return false;
-	}
+    @Override
+    public boolean isFourOfAKind() {
+
+        return false;
 
 
-	@Override
-	public boolean isStraightFlush() {
-		return false;
-	}
+    }
 
 
-	@Override
-	public HandValue getValue() {
-		HandValue handValue = new HandValue();
+    @Override
+    public boolean isFull() {
+        return false;
+    }
 
-		// Exemple for FourOfAKind ; // do for all classifiers
-		if(this.isFourOfAKind()){
-			handValue.setClassifier(HandClassifier.FOUR_OF_A_KIND);
-			handValue.setLevelValue(this.levelValue);
-			handValue.setSingleCards(this.singleCards); // or this.getsingleCards()
-			return handValue;
-		}
+
+    @Override
+    public boolean isStraightFlush() {
+        return false;
+    }
+
+
+    @Override
+    public HandValue getValue() {
+        HandValue handValue = new HandValue();
+
+        // Exemple for FourOfAKind ; // do for all classifiers
+        if (this.isFourOfAKind()) {
+            handValue.setClassifier(HandClassifier.FOUR_OF_A_KIND);
+            handValue.setLevelValue(this.levelValue);
+            handValue.setSingleCards(this.singleCards); // or this.getsingleCards()
+            return handValue;
+        }
 
         // For the flush, all singleCards are needed
 
-		return handValue;
-	}
+        return handValue;
+    }
 
 
-	@Override
-	public boolean hasCardValue(int level) {
+    @Override
+    public boolean hasCardValue(int level) {
 
-		return false;
-	}
-
-
-	@Override
-	public boolean hasAce() {
-		return false;
-	}
+        return false;
+    }
 
 
-	@Override
-	public int highestValue() {
+    @Override
+    public boolean hasAce() {
+        return false;
+    }
+
+
+    @Override
+    public int highestValue() {
         // ace might be the highest value
-		return 0;
-	}
+        return 0;
+    }
 
 
-	@Override
-	public int compareTo(IHandResolver o) {
-		return 0;
-	}
 
-	
+    @Override
+    public int compareTo(IHandResolver o) {
+        // You should reuse HandValue.compareTo()
+        return 0;
+    }
+
+
 }
