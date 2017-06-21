b747b5fc50a5e803e63aba9fa27d8c6d74249691
Nicolas Zozol <nzozol@gmail.com>
2017-06-21 12:37:17 +0200

    classic classes

---
+++ classic/src/main/java/io/robusta/java/classic/ListApp.java
@@ -0,0 +1,25 @@
+package io.robusta.java.classic;
+
+import java.util.List;
+
+/**
+ * Created by nicorama on 21/06/2017.
+ */
+public class ListApp {
+
+    List<Integer> buildList(int a, int b, int c){
+        return null;
+    }
+
+
+    boolean equality(List<Integer> list1, List<Integer> list2){
+
+        return false;
+    }
+
+    String asString (List<Integer> numbers){
+        return null;
+    }
+
+
+}
